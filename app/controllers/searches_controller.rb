6a57f5fa01a0e9446c687a65bad1a09b64244be1
Mat Brown <mat@cliqueinnyc.com>
2011-11-13 11:41:21 -0500

    Messages for book/drink not found

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,25 +5,24 @@ class SearchesController < ApplicationController
   def show
     title = params[:title]
     if title
-      book = Rails.cache.fetch("book/#{title.downcase.gsub(/\W+/, '-')}") do
+      @book = Rails.cache.fetch("book/#{title.downcase.gsub(/\W+/, '-')}") do
         results = GoodReads.search_books(:title, title)
         if results.any?
           book = GoodReads.get_book(results.first.id)
         end
       end
 
-      if book
-        if book.popular_shelves.present?
+      if @book
+        if @book.popular_shelves.present?
           shelves_by_name = {}
-          Shelf.where(:name => book.popular_shelves).where('drink_id IS NOT NULL').each do |shelf|
+          Shelf.where(:name => @book.popular_shelves).where('drink_id IS NOT NULL').each do |shelf|
             shelves_by_name[shelf.name] = shelf
           end
           if shelves_by_name.any?
             @shelf = nil
-            book.popular_shelves.each do |name|
+            @book.popular_shelves.each do |name|
               break if @shelf = shelves_by_name[name]
             end
-            @book = book
             @drink = @shelf.drink
           end
         end
