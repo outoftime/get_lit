d8dfa3470dcb79a31bd746d533b68b8b992a4eaf
Mat Brown <mat@cliqueinnyc.com>
2011-11-13 17:55:54 -0500

    Some basic (ugly) styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def dom_class_for_controller
+    params[:controller].split('/').map { |c| c.camelize }.join(' ')
+  end
+
+  def dom_id_for_action
+    params[:action].camelize
+  end
 end
