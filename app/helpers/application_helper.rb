49efa2932691986e529cd72add3adc0d574b90cf
Mat Brown <mat.a.brown@gmail.com>
2012-05-27 13:09:11 -0400

    Share links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,24 @@ module ApplicationHelper
   def dom_id_for_action
     params[:action].camelize
   end
+
+  def twitter_share_link(book, drink)
+    name = drink.try(:name) || 'dingus'
+    status = "Enjoying #{book.title} with a #{name} on the side. http://#{request.host_with_port}/#{book.title}"
+    URI::HTTP.build(
+      :host => 'twitter.com',
+      :path => '/home',
+      :query => { :status => status }.to_query
+    ).to_s
+  end
+
+  def facebook_share_link(book, drink)
+    name = drink.try(:name) || 'dingus'
+    url =  "http://#{request.host_with_port}/#{book.title}"
+    URI::HTTP.build(
+      :host => 'www.facebook.com',
+      :path => '/share.php',
+      :query => {:u => url}.to_query
+    ).to_s
+  end
 end
